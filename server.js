39a3c86192cd7274d0c68878c7226645d9f0ce1c
Patricia Stanca <patri@Patricias-MacBook-Air.local>
2018-02-21 10:43:37 +0000

    changes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
@@ -6,7 +7,7 @@ var http = require('http').Server(app);
 
 var port = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, './frontend')));
 
 http.listen(port, function() {
   console.log('listening on *:' + port);
