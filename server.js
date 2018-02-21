482f9d7c25aa222017980d6adaf7cafc64b3741c
Patricia Stanca <patri@Patricias-MacBook-Air.local>
2018-02-21 10:33:05 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+var http = require('http').Server(app);
+
+var port = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+http.listen(port, function() {
+  console.log('listening on *:' + port);
+});
