ef460a7be399cc7c69a37596d1bd03fa5ac5f5f6
Patricia Stanca <patri@Patricias-Air.connect>
2018-02-25 18:54:24 +0000

    add console log for search button

--- frontend/app.js
+++ frontend/app.js
@@ -3,6 +3,7 @@ var main = function() {
     document.getElementById('search-button').onclick = function()
     {
         location.href = document.getElementById('search-text').value;
+        console.log("location.href");
     };
 }
 
