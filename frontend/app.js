53b6c807be281ff882974f81310c9d3284f31f0f
Patricia Stanca <patri@Patricias-Air.connect>
2018-02-25 18:53:36 +0000

    add code for search button

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,9 @@
 var main = function() {
     //to do
+    document.getElementById('search-button').onclick = function()
+    {
+        location.href = document.getElementById('search-text').value;
+    };
 }
 
 $(document).ready(main);
