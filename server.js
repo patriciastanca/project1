516b0ef72e2a47a61d21518ef0408861c5546ddf
Patricia Stanca <patri@Patricias-Air.connect>
2018-02-25 19:21:29 +0000

    remove app.js and move its code to server.js

--- server.js
+++ server.js
@@ -12,3 +12,28 @@ app.use(express.static(path.join(__dirname, './frontend')));
 http.listen(port, function() {
   console.log('listening on *:' + port);
 });
+
+var main = function() {
+  
+function fillField(input,val) {
+  if(input.value == "")
+     input.value=val;
+};
+
+function clearField(input,val) {
+  if(input.value == val)
+     input.value="";
+};
+
+document.getElementById('search-button').onclick = function()
+{
+  var input_value = document.getElementById('search-text').value;
+  location.href = "https://giphy.com/" + input_value;
+
+  console.log(document.getElementById('search-text').value);
+  console.log(location.href);
+};
+
+}
+
+$(document).ready(main);
