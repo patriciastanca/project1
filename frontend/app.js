052ea3a49b96fe72bf026054589f65d3efcca32f
Patricia Stanca <patri@Patricias-Air.connect>
2018-02-25 20:23:16 +0000

    modified app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,16 +1,3 @@
-var main = function(){
-
-document.getElementById('search-button').onclick = function()
-{
-  var input_value = document.getElementById('search-text').value;
-  location.href = "https://giphy.com/" + input_value;
-
-  console.log(document.getElementById('search-text').value);
-  console.log(location.href);
-};
-}
-$(document).ready(main);
-
 function fillField(input,val) {
     if(input.value == "")
        input.value=val;
