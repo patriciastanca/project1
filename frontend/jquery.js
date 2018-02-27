3efd993274b101f6c5fb4cacc598e6e620a487be
Patricia Stanca <patri@Patricias-Air.connect>
2018-02-27 20:49:00 +0000

    test smth 3

--- frontend/jquery.js
+++
@@ -1,8 +0,0 @@
-//document.getElementById('search-button').onclick = function()
-//{
-//  var input_value = document.getElementById('search-text').value;
-//  location.href = "https://giphy.com/" + input_value;
-//
-  //console.log(document.getElementById('search-text').value);
-  //console.log(location.href);
-//};
