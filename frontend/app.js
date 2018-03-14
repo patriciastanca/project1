0bb9502345deb4edaf7746b98255a4cfc62e3fa0
Patricia Stanca <patri@Patricias-Air.connect>
2018-03-14 00:43:20 +0000

    Display gif from giphy when user inserts search value

--- frontend/app.js
+++ frontend/app.js
@@ -5,31 +5,37 @@ $(document).ready(function() {
     document.getElementById('search-button').onclick = function()
     {
       var input_value = document.getElementById('search-text').value;
-      location.href = "https://giphy.com/" + input_value;
-      document.getElementById('search-text').value = "Search for...";
-      socket.emit('query',input_value);
-
+      //location.href = "https://giphy.com/" + input_value;
       console.log(document.getElementById('search-text').value);
-      console.log(location.href);
+
+      socket.emit('query',input_value);
+      document.getElementById('search-text').value = "Search for...";
     };
 
-   
-    socket.emit('Heeeeey','Hello');
-    socket.on("Welcome", function(message){
+    socket.on('return_gif', function(message){
         console.log(message);
+        //location.href = message;
+        var gif_url = message;
+        document.getElementById('kitty').src = gif_url;
     });
+
+     //socket.emit('Heeeeey','Hello');
+   // socket.on('Welcome', function(message){
+       // console.log(message);
+   // });
 });
 
 function fillField(input,val) {
     if(input.value == "")
-       input.value=val;
+       input.value = val;
   };
   
-  function clearField(input,val) {
+function clearField(input,val) {
     if(input.value == val)
-       input.value="";
+       input.value = "";
   };
-
-  //function setDefaultValueForSearchField(){
-  //  document.getElementById('search-text').value = "Search for...";
-  //}
+  
+function displayGif(input, val) {
+    if (input.value == "Static Image")
+        input.value = val;
+};
--- server.js
+++ server.js
@@ -5,19 +5,37 @@ var app = express();
 
 var http = require('http').Server(app);
 
-var io = require("socket.io")(http);
+var io = require('socket.io')(http);
 
 var port = process.env.PORT || 3001;
 
+var GphApiClient = require ('giphy-js-sdk-core');
+client = GphApiClient("HSub9HpymwLnjDWWssQ844HLmzVTVv4n");
+
 app.use(express.static(path.join(__dirname, './frontend')));
+
 io.sockets.on("connection", function(socket){
-  console.log(socket.id); 
-  socket.on("Heeeeey", function(message){
-    console.log ("It works!");
-    socket.emit('Welcome','HELLO');
-  });
-  socket.on("query", function(message){
-    console.log (message);
+  //console.log(socket.id); 
+  //socket.on("Heeeeey", function(message){
+   // console.log("It works!");
+   // socket.emit('Welcome','HELLO');
+  //});
+
+  socket.on('query', function(message){
+    console.log(message);
+
+    //search for a gif
+    client.search('gifs', {"q": message, "limit": 1}).then((response)=> {
+      response.data.forEach((gifObject) => {
+        console.log(gifObject);
+        socket.emit('return_gif', gifObject.embed_url);
+
+        console.log("Gif name: ", gifObject.title);
+        console.log("Gif URL: ", gifObject.url);
+      });
+    })
+    .catch((err) => {
+    });
   });
 });
 
